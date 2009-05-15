af5be5425e4db8237159c2e4933bb375b007c6b2
alexandre.kunieda <alexandre.kunieda@3c6ad236-2ae1-11de-9401-6341fb89ae7c>
2009-05-15 19:57:52 +0000

    Operações com conjuntos corrigidas

--- trunk/conjunto.h
+++ trunk/conjunto.h
@@ -4,13 +4,14 @@
 typedef struct conjunto_ {
 	void *i;
 	struct conjunto_ *next;
+	int sz;
 } conjunto;
 
 typedef int (*funcao)(void*, void*);
 
 conjunto *conj_init();
 void conj_destroy(conjunto *c);
-void conj_insere(conjunto *c, void *e, funcao cmp);
+void conj_insere(conjunto *c, void *e, int sz, funcao cmp);
 conjunto *conj_interseccao(conjunto *c1, conjunto *c2, funcao cmp);
 conjunto *conj_diferenca(conjunto *c1, conjunto *c2, funcao cmp);
 
