553425a876f4bdff28b046ae6b238ea636848480
lucastdcj <lucastdcj@3c6ad236-2ae1-11de-9401-6341fb89ae7c>
2009-05-30 17:19:53 +0000

    lab3.c modificado

--- trunk/lab3.c
+++ trunk/lab3.c
@@ -24,25 +24,24 @@ void le_dados(char **arg){
     fclose(file_input[i]);
   
   printf("Leitura efetuada com sucesso!\n");
-  Pause();
+
 }
 
 void compara_tempo(){
   int i,j;
+  char c;
   int k,n1,n2;
-  double t1,t2;
+  double t1[4],t2[4];
   struct timeb ini, fim;
   
 
   printf("Digite a quantidade de consultas a realizar:\n");
   scanf("%d",&k);
+    
   n1 = k/N_IPIndiv;
   n2 = k/N_IPEspec;
   
   /* EXECUTA A CONSULTA A */
-  t1 = 0;
-  t2 = 0;
-
   ftime(&ini);
 
   for(i=0;i<N_IPIndiv;i++)
@@ -50,16 +49,79 @@ void compara_tempo(){
       historico_monitoramento_lab1(IPIndiv[i].id);
 
   ftime(&fim);
-  t1 = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+  t1[0] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
 
   ftime(&ini);
   for(i=0;i<N_IPIndiv;i++)
     for(j=0;j<n1;j++)
       historico_monitoramento_lab2(IPIndiv[i].id);
   ftime(&fim);
-  t2 = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+  t2[0] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+  
+      
+  /*EXECUTA A CONSULTA B*/
+  ftime(&ini);
+
+  for(i=0;i<N_IPIndiv;i++)
+    for(j=0;j<n1;j++)
+      ultima_captura_lab1(IPIndiv[i].id);
+
+  ftime(&fim);
+  t1[1] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+
+  ftime(&ini);
+  for(i=0;i<N_IPIndiv;i++)
+    for(j=0;j<n1;j++)
+      ultima_captura_lab2(IPIndiv[i].id);
+  ftime(&fim);
+  t2[1] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+  
+  
+  
+  /*EXECUTA A CONSULTA C*/
+  ftime(&ini);
+
+  for(i=0;i<N_IPEspec;i++)
+    for(j=0;j<n2;j++)
+      ultima_captura_peso_lab1(IPEspec[i].id);
+
+  ftime(&fim);
+  t1[2] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+
+  ftime(&ini);
+  for(i=0;i<N_IPEspec;i++)
+    for(j=0;j<n2;j++)
+      ultima_captura_peso_lab2(IPEspec[i].id);
+  ftime(&fim);
+  t2[2] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+  
+  
  
-  printf("%lf %lf\n",t1,t2);
+  /*EXECUTA A CONSULTA D*/
+  ftime(&ini);
+
+  for(i=0;i<N_IPIndiv;i++)
+    for(j=0;j<n1;j++)
+      caminho_especie_lab1(IPIndiv[i].id);
+
+  ftime(&fim);
+  t1[3] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+
+  ftime(&ini);
+  for(i=0;i<N_IPIndiv;i++)
+    for(j=0;j<n1;j++)
+      caminho_especie_lab2(IPIndiv[i].id);
+  ftime(&fim);
+  t2[3] = ((double) fim.time + ((double) fim.millitm * 0.001)) -((double) ini.time + ((double) ini.millitm * 0.001));
+  
+  
+  system("clear");
+  printf("Consulta Tempo 1(ms) Tempo 2(ms) Reducao\n");
+  for(i=0,c='a';i<4;i++,c++){    
+    printf("4.%c %10.2lf %12.2lf %11.2lf%%\n",c,1000*t1[i],1000*t2[i],100*((t1[i]-t2[i])/t1[i]));
+  }
+
+  
   Pause();
   return;
 }
