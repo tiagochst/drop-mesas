3a5972ffda445e1a5e7f86310f71110a6a81bc33
lucastdcj <lucastdcj@3c6ad236-2ae1-11de-9401-6341fb89ae7c>
2009-05-30 13:59:35 +0000

    Lab1 embutido

--- main.c
+++ main.c
@@ -21,80 +21,91 @@ FILE *FISecIndiv;
 FILE *FISecCaptu;
 
 int main(int argc, char **argv) {
-	int menu, m2, i;
-	FILE *file_input[3];
+  int menu, m2, i;
+  FILE *file_input[3];
 
-	FEspec = open_file(SEspec);
-	FIndiv = open_file(SIndiv);
-	FCaptu = open_file_bin(SCaptu);
+  FEspec = open_file(SEspec);
+  FIndiv = open_file(SIndiv);
+  FCaptu = open_file_bin(SCaptu);
 
-	indice_start(SIPrimEspec, SIPrimIndiv);
-	indice_sec_start(SISecIndiv, SISecCaptu);
-	lista_inv_start(SInvPrim, SInvSec);
+  indice_start(SIPrimEspec, SIPrimIndiv);
+  indice_sec_start(SISecIndiv, SISecCaptu);
+  lista_inv_start(SInvPrim, SInvSec);
 
-	if(argc == 1) {
-		m2 = 0;
-		while (1) {
-			if ((m2 == 0) && (!(menu = print_menu(0))))
-				break;
+  if(argc == 1) {
+    m2 = 0;
+    while (1) {
+      if ((m2 == 0) && (!(menu = print_menu(0))))
+	break;
 
-			m2 = print_menu(menu);
-			if (m2 == 0) continue;
+      m2 = print_menu(menu);
+      if (m2 == 0) continue;
 
-			if (menu == 1) {
-				if (m2 == 1) especie_insere();
-				if (m2 == 2) especie_le();
-				if (m2 == 3) especie_atualiza();
-				if (m2 == 4) especie_deleta();
-			}
-			if (menu == 2) {
-				if (m2 == 1) individuo_insere();
-				if (m2 == 2) individuo_le();
-				if (m2 == 3) individuo_atualiza();
-				if (m2 == 4) individuo_deleta();
-			}
-			if (menu == 3) {
-				if (m2 == 1) captura_insere();
-				if (m2 == 2) captura_le();
-				if (m2 == 3) captura_atualiza();
-				if (m2 == 4) captura_deleta();
-			}
-			if (menu == 4) {
-				if (m2 == 1) historico_monitoramento();
-				if (m2 == 2) ultima_captura();
-				if (m2 == 3) ultima_captura_peso();
-				if (m2 == 4) caminho_especie();
-				if (m2 == 5) busca_especie_descricao();
-			}
-		}
+      if (menu == 1) {
+	if (m2 == 1) especie_insere();
+	if (m2 == 2) especie_le();
+	if (m2 == 3) especie_atualiza();
+	if (m2 == 4) especie_deleta();
+      }
+      if (menu == 2) {
+	if (m2 == 1) individuo_insere();
+	if (m2 == 2) individuo_le();
+	if (m2 == 3) individuo_atualiza();
+	if (m2 == 4) individuo_deleta();
+      }
+      if (menu == 3) {
+	if (m2 == 1) captura_insere();
+	if (m2 == 2) captura_le();
+	if (m2 == 3) captura_atualiza();
+	if (m2 == 4) captura_deleta();
+      }
+      if (menu == 4) {
+	if (m2 == 1) historico_monitoramento_lab2();
+	if (m2 == 2) ultima_captura_lab2();
+	if (m2 == 3) ultima_captura_peso_lab2();
+	if (m2 == 4) caminho_especie_lab2();
+	if (m2 == 5) busca_especie_descricao();
+      }
+    }
+  }
+  else if(argc == 4) {
+    while ((m2 = print_menu_lab3())) {
+            
+      if(m2 == 1){
+	if (m2 == 0) continue;
+	for(i=0 ; i<3 ; i++) {
+	  
+	  file_input[i] = fopen(argv[i+1], "r");
+	  
+	  if(file_input[i] == NULL) {
+	    fprintf(stderr, "ERRO [main]: Nao foi possivel abrir um dos arquivos de input.");
+	    return 1;
+	  }
 	}
-	else if(argc == 4) {
-		for(i=0 ; i<3 ; i++) {
-			printf("<%s>\n", argv[i+1]);
+	especie_insere_lab3(file_input[0]);
+	individuo_insere_lab3(file_input[1]);
+	captura_insere_lab3(file_input[2]);
+	 
+	for(i=0 ; i<3 ; i++)
+	  fclose(file_input[i]);
 
-			file_input[i] = fopen(argv[i+1], "r");
+	printf("Leitura efetuada com sucesso!\n");
+	Pause();
+      }
+      if(m2 == 2){
+	
+      }
+    }
+  }
+  else return 1;
+  
+  lista_inv_end();
+  indice_end(SIPrimEspec, SIPrimIndiv);
+  indice_sec_end(SISecIndiv, SISecCaptu);
 
-			if(file_input[i] == NULL) {
-				fprintf(stderr, "ERRO [main]: Nao foi possivel abrir um dos arquivos de input.");
-				return 1;
-			}
-		}
-		especie_insere_lab3(file_input[0]);
-		individuo_insere_lab3(file_input[1]);
-		captura_insere_lab3(file_input[2]);
+  fclose(FCaptu);
+  fclose(FEspec);
+  fclose(FIndiv);
 
-		for(i=0 ; i<3 ; i++)
-			fclose(file_input[i]);
-	}
-	else return 1;
-
-	lista_inv_end();
-	indice_end(SIPrimEspec, SIPrimIndiv);
-	indice_sec_end(SISecIndiv, SISecCaptu);
-
-	fclose(FCaptu);
-	fclose(FEspec);
-	fclose(FIndiv);
-
-	return 0;
+  return 0;
 }
