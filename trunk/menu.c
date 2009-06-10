4de32815d443dcf742019f7d9054f3b619d1fabf
tiagochst <tiagochst@3c6ad236-2ae1-11de-9401-6341fb89ae7c>
2009-06-10 18:05:06 +0000

    Menu cores melhoras codigo

--- trunk/menu.c
+++ trunk/menu.c
@@ -1,27 +1,47 @@
 #include "menu.h"
 
 void CLEAR_SCREEN(){
-  system("clear");
-}
-
-void PRINT_YELLOW(){
-  printf("%c[%d;%dm", 0x1B, BRIGHT,YELLOW);	
-}
-
-void PRINT_BLUE(){
-  printf("%c[%d;%dm", 0x1B, BRIGHT,BLUE);	
+  printf("\e[H\e[2J"); /* printf() para limpar a tela */
 }
 
 void MENU_NO_COLOR(){
   printf("%c[%dm", 0x1B, NO_COLOR);
 }
+void PRINT_YELLOW(char texto[]){
+  printf("%c[%d;%dm", 0x1B, BRIGHT,YELLOW);	
+  printf("%s",texto);
+  MENU_NO_COLOR();
+}
+
+void PRINT_BLUE(char texto[]){
+  printf("%c[%dm", 0x1B,BLUE);	
+  printf("%s",texto);
+  MENU_NO_COLOR();
+}
+
 void MENU_WALL_START(){
-  PRINT_YELLOW();
-  printf("*");
-  PRINT_BLUE();
+  PRINT_YELLOW("*");
+ 
 }
 void MENU_WALL_END(){
-  PRINT_YELLOW();
-  printf("*\n");
-  PRINT_BLUE();
+  PRINT_YELLOW("*\n");
+ }
+
+void BLINK_RED(char texto[]){
+  printf("%c[%d;%d;%dm", 0x1B,RED,BRIGHT,BLINK_ON);
+  printf("%s\n",texto);
+  MENU_NO_COLOR();
+  
+}
+
+void PRINT_WALLS(char texto[]){
+	MENU_WALL_START();
+	printf("%s",texto);
+      	MENU_WALL_END();
+}
+
+void PRINT_WALLS_BLUE(char texto[]){
+	MENU_WALL_START();
+	PRINT_BLUE(texto);
+      	MENU_WALL_END();
 }
