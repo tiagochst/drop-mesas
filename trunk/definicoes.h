a9cfb5d51d3522103c0b55ec56328c9f17abf9c2
alexandre.kunieda <alexandre.kunieda@3c6ad236-2ae1-11de-9401-6341fb89ae7c>
2009-06-11 02:52:25 +0000

    Adicionada estrutura de conjuntos; Base da leitura dos arquivos feita

--- trunk/definicoes.h
+++ trunk/definicoes.h
@@ -6,15 +6,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "conjunto.h"
 
+extern int NFile;
+extern char** FileNames;
+extern FILE** File;
 
 /* funcoes.c */
 int print_menu();
 void Pause();
+char* muda_ext(char *file, char *nova_ext);
+FILE* Fopen(char *file, char *modo);
+void files_start();
+void files_end();
 
 /* opcao1.c */
 void opcao1();
-void gera_vetor(char *arq);
+void gera_vetor(FILE *fp, char *arq_vet);
 
 /* opcao2.c */
 void opcao2();
