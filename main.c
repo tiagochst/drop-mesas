66b292e24911ce42883a1f2531eeb71cfff341c1
alexandre.kunieda <alexandre.kunieda@3c6ad236-2ae1-11de-9401-6341fb89ae7c>
2009-06-29 03:18:05 +0000

    Alterações finais (será o fim?)

--- main.c
+++ main.c
@@ -7,15 +7,15 @@ FILE** File;
 Conjunto **FileWords;
 
 int main() {
-  int menu;
+	int menu;
 
-  files_start();
-  while ((menu = print_menu())) {
-    if (menu == 1) opcao1();
-    if (menu == 2) opcao2();
-    if (menu == 3) opcao3();
-  }
-  files_end();
+	files_start();
+	while ((menu = print_menu())) {
+		if (menu == 1) opcao1();
+		if (menu == 2) opcao2();
+		if (menu == 3) opcao3();
+	}
+	files_end();
 
-  return 0;
+	return 0;
 }
